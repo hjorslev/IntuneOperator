31d4516c01e2cefdd81601966f6b04a63615cce4
Frederik Hjorslev Nylander <fhn.it@cbs.dk>
2026-03-26 21:29:05 +0100

    fix linting

--- src/functions/public/Device/Get-IntuneDevice.ps1
+++ src/functions/public/Device/Get-IntuneDevice.ps1
@@ -36,7 +36,7 @@ function Get-IntuneDevice {
     Resolves device by name and returns summary details for each matching managed device.
 
     .EXAMPLE
-    Get-IntuneDevice -UserPrincipalName jane.doe@contoso.com
+    Get-IntuneDevice -UserPrincipalName "jane.doe@contoso.com"
 
     Returns summary details for all managed devices whose primary user is jane.doe@contoso.com.
 
