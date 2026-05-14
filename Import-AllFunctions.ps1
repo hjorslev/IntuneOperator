5d1a8ce748cd138f9043053d6baeb98264436eec
Frederik Hjorslev Nylander <fhn.it@cbs.dk>
2026-05-14 17:44:50 +0200

    add script to import all cmdlets

---
+++ Import-AllFunctions.ps1
@@ -0,0 +1,17 @@
+﻿# Dot-source alle funktioner i IntuneOperator
+# Brug: . ./Import-AllFunctions.ps1
+
+$publicFolders = Get-ChildItem -Path "$PSScriptRoot/src/functions/public" -Directory
+$privateFolder = Join-Path $PSScriptRoot 'src/functions/private'
+
+# Dot-source alle private funktioner
+Get-ChildItem -Path $privateFolder -Filter '*.ps1' | ForEach-Object {
+    . $_.FullName
+}
+
+# Dot-source alle public funktioner (også i subfolders)
+foreach ($folder in $publicFolders) {
+    Get-ChildItem -Path $folder.FullName -Recurse -Filter '*.ps1' | ForEach-Object {
+        . $_.FullName
+    }
+}
